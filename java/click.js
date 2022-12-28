bede8fd22532d1dacd283f76e9f2edaa9644a822
Евгений Смирнов <john37rus@gmail.com>
2022-12-28 03:28:58 +0300

    3:28 (28.12.2022)

---
+++ java/click.js
@@ -0,0 +1,10 @@
+const btn_send = document.getElementById("#sendValues");
+btn_send.addEventListener("click", () => {
+    user_name2 = user_name;
+    tel2 = tel;
+    email2 = email;
+    section2 = section;
+    bday2 = bday;
+    myCheck2 = myCheck;
+    report2 = report;
+});
